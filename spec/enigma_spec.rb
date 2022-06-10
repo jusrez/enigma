bc36f318bb1ff0c776f0788eee40d691ae1b3741
Justin Ramirez <jusrez528@gmail.com>
2022-06-09 20:34:46 -0400

    Add test for encrypt method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Enigma do
 
 		expect(enigma).to be_an_instance_of(Enigma)
 	end
+
+	it 'encrypts a message' do
+		enigma = Enigma.new
+
+		expect(enigma.encrypt("hello world", "02715", "040895")).to eq("keder ohulw")
+	end
 end
