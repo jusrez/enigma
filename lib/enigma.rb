3d9353829d888c78f8cf2d5bc38dc48f612ddfb5
Justin Ramirez <jusrez528@gmail.com>
2022-06-11 14:25:54 -0400

    Add generate key method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,20 @@
 class Enigma
+
+	def initialize
+	end
+
+	# def encrypt(message, key, date = Date.today.strftime("%d%m%y"))
+	#
+	# end
+
+	def generate_key
+		key_set = []
+		range = 0..9
+		num_set = range.to_a
+		while key_set.count < 5
+			key_set << num_set.sample
+		end
+		return key_set.to_s.delete(",[] ")
+	end
+
 end
