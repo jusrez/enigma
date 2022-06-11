26f3a76d1383fa48b4c33e6b6c1bb8cfd5c6346a
Justin Ramirez <jusrez528@gmail.com>
2022-06-11 14:24:53 -0400

    Add test for generate key method

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,9 +7,12 @@ RSpec.describe Enigma do
 		expect(enigma).to be_an_instance_of(Enigma)
 	end
 
-	it 'encrypts a message' do
+	it 'will generate a key if one is not provided' do
 		enigma = Enigma.new
 
-		expect(enigma.encrypt("hello world", "02715", "040895")).to eq("keder ohulw")
+		expect(enigma.generate_key).to be_a(String)
+		expect(enigma.generate_key.length).to eq(5)
 	end
+
+
 end
