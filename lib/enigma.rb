9702f206e9221434e41f339752388fb1a5832772
Justin Ramirez <jusrez528@gmail.com>
2022-06-12 23:11:30 -0400

    Add decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -16,6 +16,17 @@ class Enigma
 		return Hash(encryption: encrypted_message.join, key: key, date: date)
 	end
 
+	def decrypt(ciphertext, key, date = generate_date)
+		decrypted_message = []
+		shifts = final_shifts(key, date)
+		ciphertext_array = ciphertext.downcase.split("")
+		ciphertext_array.each.with_index do |letter, i|
+			decrypted_message << @character_set.rotate(@character_set.find_index(letter) - shifts.rotate(i)[0])[0] if @character_set.include?(letter)
+			decrypted_message << letter if @character_set.include?(letter) == false
+		end
+		return Hash(decryption: decrypted_message.join, key: key, date: date)
+	end
+
 	def five_digit_number
 		key_set = []
 		num_set = (0..9).to_a
