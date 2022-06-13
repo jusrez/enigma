829bc592aa621ef2bee6365835bdbeeee26f7275
Justin Ramirez <jusrez528@gmail.com>
2022-06-13 12:25:15 -0400

    Add code that allows runner to read messages and write to another text file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,14 @@
+require_relative './enigma.rb'
 
+message_file = File.open(ARGV[0], "r")
+read = message_file.read.chomp
+
+
+enigma = Enigma.new
+encrypted = enigma.encrypt(read, "02715", "040895")
+
+write_file = File.open(ARGV[1], "w")
+write_file.write(encrypted[:encryption])
+
+
+puts "Created '#{ARGV[1]}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
