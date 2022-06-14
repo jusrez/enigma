a6e952efd9f071ae95408339c53771476c65b951
Justin Ramirez <jusrez528@gmail.com>
2022-06-14 17:49:52 -0400

    Edit encrypt and decrypt to make methods horizontally shorter and vertically longer

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,7 +5,7 @@ class Enigma
 	include Calculable
 
 	def initialize
-		@character_set = ("a".."z").to_a << " "
+		@char_set = ("a".."z").to_a << " "
 	end
 
 	def encrypt(message, key = five_digit_number, date = generate_date)
@@ -13,8 +13,11 @@ class Enigma
 		shifts = final_shifts(key, date)
 		message_array = message.downcase.split("")
 		message_array.each.with_index do |letter, i|
-			encrypted_message << @character_set.rotate(@character_set.find_index(letter) + shifts.rotate(i)[0])[0] if @character_set.include?(letter)
-			encrypted_message << letter if @character_set.include?(letter) == false
+			if @char_set.include?(letter) == false
+				encrypted_message << letter
+			else
+				encrypted_message << @char_set.rotate(@char_set.find_index(letter) + shifts.rotate(i)[0])[0]
+			end
 		end
 		return Hash(encryption: encrypted_message.join, key: key, date: date)
 	end
@@ -24,12 +27,15 @@ class Enigma
 		shifts = final_shifts(key, date)
 		ciphertext_array = ciphertext.downcase.split("")
 		ciphertext_array.each.with_index do |letter, i|
-			decrypted_message << @character_set.rotate(@character_set.find_index(letter) - shifts.rotate(i)[0])[0] if @character_set.include?(letter)
-			decrypted_message << letter if @character_set.include?(letter) == false
+			if @char_set.include?(letter) == false
+				decrypted_message << letter
+			else
+			decrypted_message << @char_set.rotate(@char_set.find_index(letter) - shifts.rotate(i)[0])[0]
+			end 
 		end
 		return Hash(decryption: decrypted_message.join, key: key, date: date)
 	end
 
 
-	
+
 end
